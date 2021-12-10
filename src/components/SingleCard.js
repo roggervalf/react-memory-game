0ae3d77199f1f624b044e54a8d69424a57128857
Diego Cicconi <diego.cicconi@autodesk.com>
2021-12-10 13:11:27 -0300

    Added turnCounter functionality and newGame hook up

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,24 +1,23 @@
-import './SingleCard.css'
+import "./SingleCard.css";
 
-export default function SingleCard({ card, handleChoice, cardFlipped, disabled }) {    // destructure card and handleChoice props from App
+export default function SingleCard({ card, handleChoice, cardFlipped }) {
+  // destructure card and handleChoice props from App
 
-    const handleClick = () => {
-        if (!disabled) {
-            handleChoice(card);
-        }
-    }
+  const handleClick = () => {
+    handleChoice(card);
+  };
 
-    return (
-        <div className="card">
-            <div className={cardFlipped ? "flipped" : ""}>
-                <img className="front" src={card.src} alt="Card front" />
-                <img
-                    className="back"
-                    src="/img/cover.png"
-                    onClick={handleClick}
-                    alt="Card back"
-                />
-            </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <div className={cardFlipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="Card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="Card back"
+        />
+      </div>
+    </div>
+  );
 }
